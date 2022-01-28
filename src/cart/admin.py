73ee5524642589c7db9fd462b6ee25f952c47ebd
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-01-28 08:55:23 -0500

    checkout completo

--- src/cart/admin.py
+++ src/cart/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import ColourVariation, Product, Order, OrderItem,SizeVariation
+from .models import Address, ColourVariation, Product, Order, OrderItem,SizeVariation
 # Register your models here.
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display =[
+        'address_line_1',
+        'address_line_2',
+        'zip_code',
+        'city',
+        'address_type',
+    ]
+
 admin.site.register(Product)
+admin.site.register(Address,AddressAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ColourVariation)
--- src/cart/forms.py
+++ src/cart/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from.models import ColourVariation, OrderItem, Product, SizeVariation
+from.models import Address, ColourVariation, OrderItem, Product, SizeVariation
+from django.contrib.auth import get_user_model
+User=get_user_model()
 class AddToCartForm(forms.ModelForm):
     colour= forms.ModelChoiceField(queryset= ColourVariation.objects.none())
     size= forms.ModelChoiceField(queryset= SizeVariation.objects.none())
@@ -16,4 +18,58 @@ class AddToCartForm(forms.ModelForm):
         self.fields['size'].queryset=product.available_sizes.all()
 
 class AddressForm(forms.Form):
-    pass
+    shipping_addres_line_1= forms.CharField(required=False)
+    shipping_addres_line_2= forms.CharField(required=False)
+    shipping_zip_code= forms.CharField(required=False)
+    shipping_city= forms.CharField(required=False)
+
+    billing_addres_line_1= forms.CharField(required=False)
+    billing_addres_line_2= forms.CharField(required=False)
+    billing_zip_code= forms.CharField(required=False)
+    billing_city= forms.CharField(required=False)
+
+    selected_shipping_address=forms.ModelChoiceField(
+        Address.objects.none(),required=False
+    )
+
+    selected_billing_address=forms.ModelChoiceField(
+        Address.objects.none(),required=False
+    )
+    def __init__(self,*args,**kwargs):
+        user_id = kwargs.pop('user_id')
+        super().__init__(*args,**kwargs)
+        user = User.objects.get(id=user_id)
+        shipping_addres_qs =  Address.objects.filter(
+            user=user,
+            address_type ='S'
+        )
+        billing_addres_qs =  Address.objects.filter(
+            user=user,
+            address_type ='B'
+        )
+        self.fields['selected_shipping_address'].queryset=shipping_addres_qs
+        self.fields['selected_billing_address'].queryset=billing_addres_qs
+    
+    def clean(self):
+        data=self.cleaned_data
+        selected_shipping_address=data.get('selected_shipping_addtess',None)
+        if selected_shipping_address is None:
+            if not data.get('shipping_addres_line_1', None):
+                self.add_error("shipping_addres_line_1", "Please fill in this field")
+            if not data.get('shipping_addres_line_2', None):
+                self.add_error("shipping_addres_line_2", "Please fill in this field")
+            if not data.get('shipping_zip_code', None):
+                self.add_error("shipping_zip_code", "Please fill in this field")
+            if not data.get('shipping_city', None):
+                self.add_error("shipping_city", "Please fill in this field")
+           
+        selected_billing_address=data.get('selected_billing_address',None)
+        if selected_billing_address is None:
+            if not data.get('billing_addres_line_1', None):
+                self.add_error("billing_addres_line_1", "Please fill in this field")
+            if not data.get('billing_addres_line_2', None):
+                self.add_error("billing_addres_line_2", "Please fill in this field")
+            if not data.get('billing_zip_code', None):
+                self.add_error("billing_zip_code", "Please fill in this field")
+            if not data.get('billing_city', None):
+                self.add_error("billing_city", "Please fill in this field")
