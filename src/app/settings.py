a8bae31f666db15794c798381901cf085b12409f
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-01-28 20:04:43 -0500

    primera prueba para agregar usuario

--- src/app/settings.py
+++ src/app/settings.py
@@ -21,6 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'crispy_forms',
     'core',
     'cart',
@@ -87,6 +92,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 # Internationalization
